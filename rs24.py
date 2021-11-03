e2a52d4698b75ea573ebbc0c9d5d614fdbf00fb8
SashaKharlamov <alex@outlinestudio.co>
2021-11-04 00:43:26 +0500

    RS24 - parser

--- rs24.py
+++ rs24.py
@@ -1,5 +1,4 @@
-# TODO 1) обработка ошибок
-# TODO  3) парс категориии и каталога
+# TODO 1) csv
 import csv
 import os
 import zipfile
@@ -8,26 +7,26 @@ from xml.etree import ElementTree
 import glob
 
 result = []
+categories = []
 unit = {'PCE': 'шт', 'NMP': 'упак', 'MTR': 'м'}
 
 
-# def connect_to_ftp_data():
-#     file_name = "result.xml"
-#     ftp = FTP()
-#     ftp.connect(host='service.russvet.ru', port=21021)
-#     ftp.login(user='Samoletdevelopment', passwd='Gn6pYJ8U')
-#     ftp.cwd('pricat/ARCHIVE')
-#     latest_file = ftp.nlst()[0]
-#
-#     download_data(ftp, latest_file, file_name)
-#     gather_data(file_name)
-#
-#     ftp.quit()
+def connect_to_ftp_data():
+    file_name = "result.xml"
+    ftp = FTP()
+    ftp.connect(host='service.russvet.ru', port=21021)
+    ftp.login(user='Samoletdevelopment', passwd='Gn6pYJ8U')
+    ftp.cwd('pricat/ARCHIVE')
+    latest_file = ftp.nlst()[0]
+
+    download_data(ftp, latest_file, file_name)
+    gather_data(file_name)
+
+    ftp.quit()
 
 
 def connect_to_ftp_structure():
-    file_name = "structure.zip"
-
+    file_name = 'structure.zip'
     ftp = FTP()
     ftp.connect(host='service.russvet.ru', port=21021)
     ftp.login(user='prodat', passwd='bT6tsv3')
@@ -35,7 +34,7 @@ def connect_to_ftp_structure():
     latest_file = ftp.nlst()[-1]
 
     download_data(ftp, latest_file, file_name)
-    unzip_structure_file()
+    unzip_structure_file(file_name)
 
     ftp.quit()
 
@@ -46,29 +45,37 @@ def download_data(ftp, latest_file, file_name):
         file.close()
 
 
-def unzip_structure_file():
-    with zipfile.ZipFile('structure.zip', 'r') as zip_ref:
+def unzip_structure_file(file_name):
+    with zipfile.ZipFile(file_name, 'r') as zip_ref:
         zip_ref.extractall()
 
 
+def load_categories():
+    for file in glob.glob('PRODAT*.xml'):
+        structure = ElementTree.parse(file)
+        desc = structure.findall("DocDetail")
 
+        for item in desc:
+            id_catalog = item.find("ParentProdGroup").text
+            id_category = item.find("ProductGroup").text
+            code = item.find("SenderPrdCode").text
+            categories.append({'code': code, 'catalog': id_catalog, 'category': id_category})
 
-connect_to_ftp_structure()
-
-
-# connect_to_ftp_data()
+        os.remove(file)
 
 
 def gather_data(file_name):
     data = ElementTree.parse(file_name)
     product = data.findall("DocDetail")
 
+    load_categories()
+
     for item in product:
         title = item.find("ProductName").text  # Наименование товара
         price = float(item.find("RetailPrice").text)  # Базовая цена товара с НДС
         discount_price = float(item.find("CustPrice").text)  # Цена клиента с НДС
         product_link = "-1"  # Ссылка на товар
-        inventory = float(item.find("SumQTY").text)  # Количество товара
+        inventory = int(item.find("SumQTY").text.split('.')[0])  # Количество товара
         if inventory > 0:
             stock = 1  # Наличие
         else:
@@ -78,23 +85,24 @@ def gather_data(file_name):
         id_site = 'RS24.ru'  # Сайт
         id_stock_name = "-1"  # Название акции
         id_unit = unit.get(item.find("UOM").text)  # Ед. измерения
-        id_city = 'all'  # Город
+        id_city = 'Москва'  # Город
+
+        for cat in categories:
+            if cat['code'] == article:
+                id_category = cat.get('catalog')  # Категория
+                id_catalog = cat.get('category')  # Каталог
+                categories.append({'code': cat, 'id_category': id_category, 'id_catalog': id_catalog})
+
+                result.append({'title': title, 'price': price, 'discount_price': discount_price,
+                               'product_link': product_link,
+                               'stock': stock, 'is_order': is_order, 'inventory': inventory, 'article': article,
+                               'id_category': id_category, 'id_site': id_site, 'id_catalog': id_catalog,
+                               'id_stock_name': id_stock_name, 'id_unit': id_unit, 'id_city': id_city})
 
 
-#TODO CATEG
-        # for file in glob.glob('PRODAT*.xml'):
-        #     structure = ElementTree.parse(file)
-        #     desc = structure.findall("DocDetail")
-        #
-        #     for rec in desc:
-        #         id_catalog = item.find("ParentProdGroup").text
-        #         id_category = item.find("ProductGroup").text
-
-        result.append({'title': title, 'price': price, 'discount_price': discount_price,
-                       'product_link': product_link,
-                       'stock': stock, 'is_order': is_order, 'inventory': inventory, 'article': article,
-                       'id_category': id_category, 'id_site': id_site, 'id_catalog': id_catalog,
-                       'id_stock_name': id_stock_name, 'id_unit': id_unit, 'id_city': id_city})
+connect_to_ftp_structure()
+connect_to_ftp_data()
+print(result)
 
 # def result_to_csv(result):
 #     keys = result[0].keys()
